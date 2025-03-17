49892142888a2639d83b3137d62f3c3139a3c487
Ethan-2525 <Cbdeleon1216@icloud.com>
2025-03-16 22:30:07 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,41 @@
+//this is called the HelloWorld class
+public class HelloWorld {
+    /*
+    *What does the main() method represent in a Java program?
+    * It represents the entry point
+     */
+        //this is the main method
+        public static void main(String[] args) {
+    
+            /*
+            Why is System.out.println() used to display output?
+            *Because it tells the to computer to print out what we type for us to see
+             */
+            //this will print out "Hello world"
+            System.out.println("Hello, World");
+    
+            //this will print out my name
+            System.out.println("Ethan DeLeon");
+    
+            //this will print out my favorite food
+            System.out.println("Wings");
+    
+            //this will print out the person I want to meet
+            System.out.println("Micheal Irvin");
+    
+            //this will print out my least favorite chore
+            System.out.println("Taking out the trash");
+    
+            //this will print out what I would rather be doing besides the lab
+            System.out.println("Playing on my Ps5");
+    
+            /*
+            *What is the purpose of comments in the program?
+            *Help explain what the code does
+            * useful for debugging
+            * the compiler ignores comments
+             */
+    
+        }
+    }
+    
